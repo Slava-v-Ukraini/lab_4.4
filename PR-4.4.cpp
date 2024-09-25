7e235c7334b066f5f25fe65a3ebe73fe5cd5b484
myroslav.holub.ri.2024@lpnu.ua <myroslav.holub.ri.2024@lpnu.ua>
2024-09-25 09:49:24 +0300

    first commit

---
+++ PR-4.4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double x, y, xp, xk, R, dx;
+	cout << " R = ";  cin >> R;
+	cout << " xp = ";  cin >> xp;
+	cout << " xk = ";  cin >> xk;
+	cout << " dx = ";  cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	xp = -4;
+	x = xp;
+	x += dx;
+
+	if (x <= -2)
+		y = x + 3;
+	else
+		if (-2 < x && x <= 4)
+			y = ((-R * -1) / 6) * x - ((R - 2) / 3);
+		else
+			if (4 < x && x <= 8 - R)
+				y = -R;
+			else
+				if (8 - R < x && x <= 8 + R)
+					y = sqrt(-(R * R - (x - 8) * (x + 8) - R));
+				else
+					y = -R;
+	cout << "|" << setw(7) << setprecision(2) << x
+		<< " |" << setw(10) << setprecision(3) << y
+		<< " |" << endl;
+	cout << "---------------------------" << endl;
+	return 0;
+}
